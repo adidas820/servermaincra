b22752787854f2568ae2587448b900fab966dbd1
adidas820 <adaialvarez2006@gmail.com>
2025-03-29 10:48:25 +0100

    Commit cerrar -2025-03-29

--- cerrarr.bat
+++ cerrarr.bat
@@ -1,4 +1,4 @@
-taskkill /im java.exe /f
+taskkill /im java.exe 
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%I
 set date=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
 git add .
