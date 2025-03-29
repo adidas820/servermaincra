4d90f2019ef1dccf7f36fee8eeb216371c76c372
adidas820 <adaialvarez2006@gmail.com>
2025-03-29 10:51:32 +0100

    Commit cerrar -2025-03-29

--- cerrarr.bat
+++ cerrarr.bat
@@ -2,7 +2,7 @@
 taskkill /im java.exe 
 
 echo Espera a que se cierre lol
-timeout /t 15 /nobreak >nul
+timeout /t 10 /nobreak >nul
 
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%I
 set date=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
