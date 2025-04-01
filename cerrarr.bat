2b32e575846e19938f1aa988e0a6cecdd284f83f
Marek8007 <135265648+Marek8007@users.noreply.github.com>
2025-04-01 19:10:29 +0200

    Commit cerrar -2025-04-01

--- cerrarr.bat
+++ cerrarr.bat
@@ -8,6 +8,6 @@ for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value ^| find "="
 set date=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
 git add .
 git commit -m "Commit cerrar -%date%"
-git push -u origin main
+git push -u origin main -f
 
 pause
